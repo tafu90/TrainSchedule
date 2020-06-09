9b359c6f9c39a07f9b308e27d2b404ca3ee13d0f
Stanislav Kostov <tafu90@gmail.com>
2020-06-09 19:54:07 +0300

    Create the project

---
+++ TrainSchedule/TrainSchedule/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  TrainSchedule
+//
+//  Created by Stanislav Kostov on 09/06/2020.
+//  Copyright © 2020 Stanislav Kostov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
